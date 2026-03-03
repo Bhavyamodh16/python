07c36c3c61ba42ed8cfa8fb009ade9bce6601d18
Bhavya Modh <bhavyamodh162@gmail.com>
2026-03-03 05:56:23 +0000

    add data_types.py

---
+++ data_types.py
@@ -0,0 +1,23 @@
+int =2
+print(int,type(int),"\n")
+a=22.4
+print(a,type(a),"\n")
+c="bhavya"
+print(c,type(c),"\n")
+d=True
+print(d,type(d),"\n")
+e=["ramesh",2]
+print(e,type(e),"\n")
+f=("ramesh",2,"ramesh")
+print(f,type(f),"\n")
+e.append("sushma")
+print(e,"\n")
+# e.extend([3])
+# print(type(e))
+e.insert(0,"bhavesh")
+print(e,"\n")
+e.pop(1)
+print(e)
+g={"name":"Bhavya","surname":"Modh"}
+print(g["name"])
+print(g["surname"])
