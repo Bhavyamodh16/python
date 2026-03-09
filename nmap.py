238e3cacc554f6f1b8395f7dc21ed7ef4068fe78
Bhavya Modh <bhavyamodh162@gmail.com>
2026-03-09 05:32:27 +0000

    add nmap.py

--- nmap.py
+++ nmap.py
@@ -3,4 +3,9 @@ print("enter the ip addr \n")
 ip=input()
 print("enter the port \n")
 port=input()
-subprocess.run(["nmap","192.168.121.128","-p","8383","-sT","-sV","-O"])
+scan=(input("enter scan type \n 1=SYN Scan \n 2=TCP Scan\n"))
+if scan==1:
+    subprocess.run(["nmap","192.168.121.128","-p","8383","-sS","-sV","-O"])
+else:
+    subprocess.run(["nmap","192.168.121.128","-p","8383","-sT","-sV","-O"])
+    
