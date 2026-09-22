4a36e7f3cb0f79857bd3e9985c64019cd8571f54
Bhavya Modh <bhavyamodh162@gmail.com>
2026-09-22 12:56:40 +0000

    add user defined function.py

--- user defined functin.py
+++ user defined functin.py
@@ -1,1 +1,12 @@
+def details():
+    print("The last date of paying the bill is this week")
+    print("After Deadline you need to pay Rs:1000")
+    print("Pay Your bill on time")
+
+for i in range(3):
+    a=input("enter your name")
+    n=int(input("Enter the Electricity units consumed"))
+    bill=10*n
+    print("You need to pay",bill)
+    
 
